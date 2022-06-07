2775c0c41db9fc68c6b1a0971a17302205751fd5
Ezeoku Ekene <git config --global user.name Ezeoku>
2022-06-06 19:48:45 -0700

    Programming in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 
+
+
 /**
- *   *   * main - main block
- *    *    *
- *     * Return: Always 0 (success)
- *      **/
+ *
+ *  * main - main block
+ *
+ *   * Return: 0
+ *
+ *    */
+
 int main(void)
+
 {
 
-	    puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-	        return (0);
+	return (0);
 
 }
