5df46b30cbc10b635aa464b90cd540ecea952e38
Ezeoku Ekene <git config --global user.name Ezeoku>
2022-06-08 21:22:49 -0700

    Functions nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,38 @@
+nclude "main.h"
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: 0
+ *
+ *     *
+ *
+ *      */
+
+int main(void)
+
+{
+
+		char string[8] = "_putchar";
+
+			int n = 0;
+
+
+
+				while (n < 8)
+
+						{
+
+									_putchar(string[n]);
+
+											++n;
+
+												}
+
+					_putchar(10);
+
+						return (0);
+
