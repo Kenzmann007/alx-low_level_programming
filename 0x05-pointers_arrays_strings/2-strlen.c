4ca9b3d15ba7afe715271cc19e4026e711a6fa0f
Ezeoku Ekene <git config --global user.name Ezeoku>
2022-06-13 18:04:10 -0700

    Pointers

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,20 +2,25 @@
 
 /**
  * _strlen - lenght of a string
- * 0s: A pointer to an int that will be changed/updated
+ * @s: A pointer to an int that will be changed/updated
  *
  * Return: void that means our answer is correct
  */
 
 int _strlen(char *s)
-{
-int u;
 
-u = 0;
-}
-while (s[u] != '\0')
 {
-u++;
-}
-return (u);
+
+	int n = 0;
+
+	while (s[n] != '\0')
+
+	{ 
+
+		n++;
+
+	}
+
+	return (n);
+
 }
